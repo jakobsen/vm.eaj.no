3ad575b6050d83bb9308869f6b8f8577b0e2eda0
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-14 11:31:43 +0200

    Landingsside: Legg til logg inn med google-knapp

--- test/tipping_web/controllers/page_controller_test.exs
+++ test/tipping_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TippingWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ ~s/id="g_id_onload"/
   end
 end
