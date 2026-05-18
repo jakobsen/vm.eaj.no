4d8918f1cc066f4cc015b7757465ecd6174c728a
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-18 22:09:17 +0200

    Landingsside: Et slags utkast

--- lib/tipping_web/controllers/page_html.ex
+++ lib/tipping_web/controllers/page_html.ex
@@ -7,4 +7,16 @@ defmodule TippingWeb.PageHTML do
   use TippingWeb, :html
 
   embed_templates "page_html/*"
+
+  attr :label, :string, required: true
+  attr :points, :integer, required: true
+
+  defp rule_row(assigns) do
+    ~H"""
+    <li class="flex justify-between bg-gray-100 rounded px-3 py-2 border border-gray-300">
+      <span>{@label}</span>
+      <span class="font-semibold">{@points} poeng</span>
+    </li>
+    """
+  end
 end
