ee5b026cb5a19ee86072afdab1563bfa932f6000
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-18 08:33:25 +0200

    WorldCup: Renskriv matches_with_bets_grouped_by_day

--- lib/tipping/world_cup.ex
+++ lib/tipping/world_cup.ex
@@ -30,12 +30,7 @@ defmodule Tipping.WorldCup do
   def matches_with_bets_grouped_by_day(%Accounts.User{} = user) do
     list_matches_with_bets(user)
     |> Enum.map(fn entry ->
-      match = entry.match
-
-      %{
-        entry
-        | match: %{match | kickoff_at: DateTime.shift_zone!(match.kickoff_at, "Europe/Oslo")}
-      }
+      put_in(entry.match.kickoff_at, DateTime.shift_zone!(entry.match.kickoff_at, "Europe/Oslo"))
     end)
     |> Enum.group_by(&DateTime.to_date(&1.match.kickoff_at))
     |> Map.to_list()
