5bdc74cc112e70abae52f0271b35bfc1cfae449d
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-21 20:44:34 +0200

    Landingsside: Ny om-seksjon

--- lib/tipping_web/components/home_components.ex
+++ lib/tipping_web/components/home_components.ex
@@ -31,6 +31,93 @@ defmodule TippingWeb.HomeComponents do
     """
   end
 
+  def about_section(assigns) do
+    assigns =
+      assigns
+      |> assign(
+        :wrapper_class,
+        "mb-20 px-2.5 leading-[1.5] tracking-[1%]"
+      )
+      |> assign(:header_class, "font-bold text-xl leading-none mb-5")
+
+    ~H"""
+    <section class={[@wrapper_class, "pt-42"]}>
+      <p class="mb-5">Konseptet er enkelt.</p>
+      <p class="mb-20">
+        Logg inn med en bedriftskonto, legg inn hva resultatet kommer til å bli i VM-kampene,
+        og vinn heder og ære.
+      </p>
+
+      <h2 class="large-heading mb-15">Regler</h2>
+      <ol>
+        <.rule number={1}>Siste frist for å tippe på en kamp er 10 minutter før avspark.</.rule>
+        <.rule number={2}>Poeng beregnes som følger:</.rule>
+        <.points_table />
+        <.rule number={3}>
+          Å delta er gratis. Premie er som nevnt heder og ære. Eventuelt
+          muligheten til å skryte en liten stund. Det får dere avklare internt,
+          tenker jeg.
+        </.rule>
+      </ol>
+    </section>
+
+    <section class={@wrapper_class}>
+      <h2 class={@header_class}>Litt om personvern og sånn</h2>
+      <p>
+        Når du logger inn lagrer vi en unik identfikator som vi får fra Google,
+        navnet ditt, og domenet til e-posten din (det etter <code>@</code>-tegnet).
+        Dette er de tre tingene siden trenger for å fungere, så mer enn det tar
+        vi ikke vare på.
+      </p>
+    </section>
+
+    <section class={@wrapper_class}>
+      <h2 class={@header_class}>Noe du vil melde?</h2>
+      <p>
+        Tilbakemeldinger, spørsmål, ris, ros, varme tanker og blomster kan sendes til <code>post@eaj.no</code>.
+      </p>
+    </section>
+    """
+  end
+
+  attr :number, :integer
+  slot :inner_block
+
+  defp rule(assigns) do
+    ~H"""
+    <li class="flex gap-5 px-2.5 py-5">
+      <span class="font-extrabold text-[1.875rem] opacity-20">{@number}</span>
+      <span class="min-h-[45px] flex items-center">{render_slot(@inner_block)}</span>
+    </li>
+    """
+  end
+
+  defp points_table(assigns) do
+    assigns =
+      assign(assigns, :class, [
+        "py-3 px-5 flex justify-between bg-[#27308366]",
+        "first:rounded-tr-[20px]",
+        "last:rounded-bl-[20px]"
+      ])
+
+    ~H"""
+    <ol class="flex flex-col gap-0.5">
+      <li class={@class}>
+        <span>Korrekt resultat</span>
+        <span>3&nbsp;p</span>
+      </li>
+      <li class={@class}>
+        <span>Korrekt målforskjell</span>
+        <span>2&nbsp;p</span>
+      </li>
+      <li class={@class}>
+        <span>Korrekt vinner</span>
+        <span>1&nbsp;p</span>
+      </li>
+    </ol>
+    """
+  end
+
   defp hero_top_decoration(assigns) do
     ~H"""
     <div class="h-[230px] w-screen bg-[#212a82] rounded-br-[50%] absolute top-0 left-0" />
