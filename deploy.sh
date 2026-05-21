e7f332c361597eb7a018eb419ddd86b4dfb4d105
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-21 16:44:24 +0200

    Mix release

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,18 @@
 
 set -e
 
-# Setup
 git pull origin main
 
-set -a
-source "./.env"
-set +a
 
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 MIX_ENV=prod mix assets.deploy
-MIX_ENV=prod mix ecto.migrate
 
-# Run the server
+MIX_ENV=prod mix release --overwrite
+
+set -a
+source "./.env"
+set +a
+_build/prod/rel/tipping/bin/migrate
+
 systemctl restart vm_tipping
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\tipping" eval Tipping.Release.migrate
