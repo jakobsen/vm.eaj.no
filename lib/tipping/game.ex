09d8bac1231bb909ef922b3e8e3513d072039dd4
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-23 15:08:15 +0200

    Game: Det er ikke mulig å spille på en kamp hvor avspark er om 10 minutter eller mindre

--- lib/tipping/game.ex
+++ lib/tipping/game.ex
@@ -4,12 +4,25 @@ defmodule Tipping.Game do
   alias Tipping.Repo
   alias Tipping.WorldCup
 
-  def place_bet(%Accounts.User{} = user, %WorldCup.Match{} = match, params) do
-    %Game.Bet{user_id: user.id, match_id: match.id}
-    |> Game.Bet.changeset(params)
-    |> Repo.insert(
-      on_conflict: {:replace, [:home_score, :away_score, :updated_at]},
-      conflict_target: [:user_id, :match_id]
-    )
+  def place_bet(
+        %Accounts.User{} = user,
+        %WorldCup.Match{} = match,
+        params,
+        now \\ DateTime.utc_now()
+      ) do
+    if match_locked?(match, now) do
+      {:error, :match_locked}
+    else
+      %Game.Bet{user_id: user.id, match_id: match.id}
+      |> Game.Bet.changeset(params)
+      |> Repo.insert(
+        on_conflict: {:replace, [:home_score, :away_score, :updated_at]},
+        conflict_target: [:user_id, :match_id]
+      )
+    end
+  end
+
+  def match_locked?(%WorldCup.Match{} = match, now \\ DateTime.utc_now()) do
+    DateTime.diff(match.kickoff_at, now, :second) <= 600
   end
 end
--- test/tipping/game_test.exs
+++ test/tipping/game_test.exs
@@ -1,45 +1,140 @@
 defmodule Tipping.GameTest do
+  alias Tipping.WorldCup
   use Tipping.DataCase
 
-  import Ecto.Query
   import Tipping.AccountsFixtures
 
+  alias Tipping.Repo
   alias Tipping.Game
-  alias Tipping.WorldCup
-
-  setup do
-    user = user_fixture()
-    match = Repo.one(from m in WorldCup.Match, limit: 1)
-
-    %{user: user, match: match}
-  end
+  alias Tipping.WorldCup.Match
 
   describe "place_bet/3" do
-    test "a user can place a bet", %{user: user, match: match} do
-      assert {:ok, bet} = Game.place_bet(user, match, %{home_score: 2, away_score: 1})
+    setup do
+      user = user_fixture()
+
+      [home_team, away_team] = WorldCup.list_teams() |> Enum.take(2)
+
+      match =
+        %Match{
+          kickoff_at: ~U[2026-06-11 19:00:00Z],
+          stage: "Gruppe A",
+          home_team_id: home_team.id,
+          away_team_id: away_team.id
+        }
+        |> Repo.insert!()
+
+      %{
+        user: user,
+        match: match,
+        half_hour_before_kickoff: ~U[2026-06-11 18:30:00Z],
+        half_hour_after_kickoff: ~U[2026-06-11 19:30:00Z]
+      }
+    end
+
+    test "a user can place a bet", %{
+      user: user,
+      match: match,
+      half_hour_before_kickoff: half_hour_before_kickoff
+    } do
+      assert {:ok, bet} =
+               Game.place_bet(
+                 user,
+                 match,
+                 %{home_score: 2, away_score: 1},
+                 half_hour_before_kickoff
+               )
+
       assert bet.user_id == user.id
       assert bet.match_id == match.id
       assert bet.home_score == 2
       assert bet.away_score == 1
     end
 
-    test "placing a second bet updates the first", %{user: user, match: match} do
-      {:ok, first_bet} = Game.place_bet(user, match, %{home_score: 1, away_score: 0})
-      assert {:ok, second_bet} = Game.place_bet(user, match, %{home_score: 1, away_score: 2})
+    test "placing a second bet updates the first", %{
+      user: user,
+      match: match,
+      half_hour_before_kickoff: half_hour_before_kickoff
+    } do
+      {:ok, first_bet} =
+        Game.place_bet(user, match, %{home_score: 1, away_score: 0}, half_hour_before_kickoff)
+
+      assert {:ok, second_bet} =
+               Game.place_bet(
+                 user,
+                 match,
+                 %{home_score: 1, away_score: 2},
+                 half_hour_before_kickoff
+               )
+
       assert first_bet.id == second_bet.id
       assert second_bet.home_score == 1
       assert second_bet.away_score == 2
     end
 
-    test "a bet cannot contain negative goals", %{user: user, match: match} do
-      assert {:error, _} = Game.place_bet(user, match, %{home_score: -1, away_score: 0})
-      assert {:error, _} = Game.place_bet(user, match, %{home_score: 0, away_score: -1})
+    test "a bet cannot contain negative goals", %{
+      user: user,
+      match: match,
+      half_hour_before_kickoff: half_hour_before_kickoff
+    } do
+      assert {:error, _} =
+               Game.place_bet(
+                 user,
+                 match,
+                 %{home_score: -1, away_score: 0},
+                 half_hour_before_kickoff
+               )
+
+      assert {:error, _} =
+               Game.place_bet(
+                 user,
+                 match,
+                 %{home_score: 0, away_score: -1},
+                 half_hour_before_kickoff
+               )
     end
 
-    test "missing scores default to 0", %{user: user, match: match} do
-      assert {:ok, bet} = Game.place_bet(user, match, %{})
+    test "missing scores default to 0", %{
+      user: user,
+      match: match,
+      half_hour_before_kickoff: half_hour_before_kickoff
+    } do
+      assert {:ok, bet} = Game.place_bet(user, match, %{}, half_hour_before_kickoff)
       assert bet.home_score == 0
       assert bet.away_score == 0
+    end
+
+    test "a bet cannot be placed after kickoff", %{
+      user: user,
+      match: match,
+      half_hour_after_kickoff: half_hour_after_kickoff
+    } do
+      assert {:error, :match_locked} =
+               Game.place_bet(
+                 user,
+                 match,
+                 %{home_score: 1, away_score: 2},
+                 half_hour_after_kickoff
+               )
+    end
+  end
+
+  describe "match_locked?/2" do
+    test "a match with kickoff more than 10 minutes in the future is not locked" do
+      match = %Match{kickoff_at: ~U[2026-06-11 19:00:00Z]}
+      now = ~U[2026-06-11 18:30:00Z]
+      refute Game.match_locked?(match, now)
+    end
+
+    test "a match with kickoff exactly 10 minutes in the future is locked" do
+      match = %Match{kickoff_at: ~U[2026-06-12 20:00:00Z]}
+      now = ~U[2026-06-12 19:50:00Z]
+      assert Game.match_locked?(match, now)
+    end
+
+    test "a match with kickoff time in the past is locked" do
+      match = %Match{kickoff_at: ~U[2026-07-01 15:00:00Z]}
+      now = ~U[2026-07-02 00:00:00Z]
+      assert Game.match_locked?(match, now)
     end
   end
 end
