c13ef15fa9e22d46321a5f93481f608b93ee3a7a
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-23 10:37:19 +0200

    Landingsside: Fjern ubrukt komponent

--- lib/tipping_web/controllers/page_html.ex
+++ lib/tipping_web/controllers/page_html.ex
@@ -9,16 +9,4 @@ defmodule TippingWeb.PageHTML do
   import TippingWeb.HomeComponents
 
   embed_templates "page_html/*"
-
-  attr :label, :string, required: true
-  attr :points, :integer, required: true
-
-  defp rule_row(assigns) do
-    ~H"""
-    <li class="flex justify-between bg-gray-100 rounded px-3 py-2 border border-gray-300">
-      <span>{@label}</span>
-      <span class="font-semibold">{@points} poeng</span>
-    </li>
-    """
-  end
 end
