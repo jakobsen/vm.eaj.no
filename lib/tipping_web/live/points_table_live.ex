bc01a46eab554da473758a9dad27e0f80b132d05
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-24 10:56:12 +0200

    Toppliste: Vis en CTA om en er eneste medlem

--- lib/tipping_web/live/points_table_live.ex
+++ lib/tipping_web/live/points_table_live.ex
@@ -12,7 +12,13 @@ defmodule TippingWeb.PointsTableLive do
       |> Game.organization_scoreboard()
       |> prepare_table()
 
-    {:ok, socket |> assign(:body_class, "bg-dark-blue") |> assign(:scores, scores)}
+    all_alone? = match?([_], scores)
+
+    {:ok,
+     socket
+     |> assign(:body_class, "bg-dark-blue")
+     |> assign(:scores, scores)
+     |> assign(:all_alone?, all_alone?)}
   end
 
   @impl true
@@ -20,9 +26,34 @@ defmodule TippingWeb.PointsTableLive do
     ~H"""
     <Layouts.app current_page={:tabell} flash={@flash}>
       <h1 class="large-heading mb-10">Tabell</h1>
-      <p class="mb-3">Stillingen i {@user.organization} så langt:</p>
+      <p class="mb-3">Stillingen i {@user.organization} så langt.</p>
       <.scoreboard scores={@scores} />
+      <div :if={@all_alone?} class="mt-10">
+        <p class="mb-5">
+          Litt ensomt her? Hvorfor ikke dele lenken med noen kollegaer, fiender eller venner?
+        </p>
+        <button
+          class="mx-auto p-2 w-38 flex justify-center items-baseline gap-2 rounded border hover:bg-white/10 action:bg-black/10"
+          id="copy-link"
+          data-clipboard-text={url(~p"/")}
+        >
+          Kopier lenke <.icon name="hero-document-duplicate" />
+        </button>
+      </div>
     </Layouts.app>
+    <script>
+      const copyButton = document.querySelector("#copy-link");
+      const originalHtml = copyButton.innerHTML;
+
+      copyButton.addEventListener("click", () => {
+        navigator.clipboard.writeText(copyButton.dataset.clipboardText).then(() => {
+          copyButton.innerText = "Kopiert!";
+          setTimeout(() => {
+            copyButton.innerHTML = originalHtml;
+          }, 2000);
+        });
+      });
+    </script>
     """
   end
 
