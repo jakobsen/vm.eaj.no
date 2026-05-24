895ac4102e197c18c3613827626a6647721021a6
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-24 09:51:56 +0200

    Toppliste: Funksjon for å finne folks rangering
    Vi må håndtere uavgjort

--- lib/tipping_web/live/points_table_live.ex
+++ lib/tipping_web/live/points_table_live.ex
@@ -1,6 +1,8 @@
 defmodule TippingWeb.PointsTableLive do
   use TippingWeb, :live_view
 
+  alias Tipping.Game
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, assign(socket, :body_class, "bg-dark-blue")}
@@ -13,5 +15,31 @@ defmodule TippingWeb.PointsTableLive do
       Hei {@user.name}!
     </Layouts.app>
     """
+  end
+
+  @doc """
+  Adds a position attribute to a list of maps containing a points attribute.
+  The maps are expected to be sorted in descending order by points.
+  Handles ties.
+  """
+  def prepare_table([]), do: []
+
+  def prepare_table(scores) do
+    {updated_scores, _acc} =
+      scores
+      |> Enum.with_index(1)
+      |> Enum.map_reduce(%{points: :infinity}, fn {row, idx}, prev_row ->
+        position = if row.points == prev_row.points, do: prev_row.position, else: idx
+
+        updated_row =
+          row
+          |> Map.put(:position, position)
+          |> Map.put(:name, row.user.name)
+          |> Map.delete(:user)
+
+        {updated_row, updated_row}
+      end)
+
+    updated_scores
   end
 end
---
+++ test/tipping_web/live/points_table_live_test.exs
@@ -0,0 +1,64 @@
+defmodule TippingWeb.PointsTableLiveTest do
+  use Tipping.DataCase
+
+  import Tipping.AccountsFixtures
+
+  alias TippingWeb.PointsTableLive
+
+  describe "prepare_table/1" do
+    setup do
+      %{user: user_fixture()}
+    end
+
+    test "adds a position attribute in ascending order to a list of descending points", %{
+      user: user
+    } do
+      assert [%{position: 1}, %{position: 2}, %{position: 3}] =
+               PointsTableLive.prepare_table([
+                 %{points: 3, user: user},
+                 %{points: 2, user: user},
+                 %{points: 1, user: user}
+               ])
+    end
+
+    test "handles ties", %{user: user} do
+      assert [%{position: 1}, %{position: 1}, %{position: 3}] =
+               PointsTableLive.prepare_table([
+                 %{points: 3, user: user},
+                 %{points: 3, user: user},
+                 %{points: 1, user: user}
+               ])
+    end
+
+    test "preserves points", %{user: user} do
+      assert [%{points: 3}, %{points: 3}, %{points: 1}] =
+               PointsTableLive.prepare_table([
+                 %{points: 3, user: user},
+                 %{points: 3, user: user},
+                 %{points: 1, user: user}
+               ])
+    end
+
+    test "removes the user attribute", %{user: user} do
+      prepared_table =
+        PointsTableLive.prepare_table([
+          %{points: 3, user: user},
+          %{points: 3, user: user},
+          %{points: 1, user: user}
+        ])
+
+      assert Enum.all?(prepared_table, &(Map.fetch(&1, :user) == :error))
+    end
+
+    test "adds a name to each row" do
+      first_user = user_fixture(%{name: "Navn Navnesen"})
+      second_user = user_fixture(%{name: "Test Testesen"})
+
+      assert [%{name: "Navn Navnesen"}, %{name: "Test Testesen"}] =
+               PointsTableLive.prepare_table([
+                 %{points: 3, user: first_user},
+                 %{points: 3, user: second_user}
+               ])
+    end
+  end
+end
