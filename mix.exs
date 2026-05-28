a266f788f15b8ac172c199d7af11215dc5bc182b
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-28 21:12:48 +0200

    Frontend: Installer motion

--- mix.exs
+++ mix.exs
@@ -88,7 +88,11 @@ defmodule Tipping.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.setup": [
+        "tailwind.install --if-missing",
+        "esbuild.install --if-missing",
+        "cmd --cd assets pnpm i --frozen-lockfile"
+      ],
       "assets.build": ["compile", "tailwind tipping", "esbuild tipping"],
       "assets.deploy": [
         "tailwind tipping --minify",
