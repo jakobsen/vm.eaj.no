e89dc37981087c257671ddd192200bb5fb20248a
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-05-28 16:09:28 +0200

    Repo: Hjelpefunksjon for count-aggregering

--- lib/tipping/repo.ex
+++ lib/tipping/repo.ex
@@ -2,4 +2,8 @@ defmodule Tipping.Repo do
   use Ecto.Repo,
     otp_app: :tipping,
     adapter: Ecto.Adapters.SQLite3
+
+  def count(queryable, opts \\ []) do
+    aggregate(queryable, :count, opts)
+  end
 end
