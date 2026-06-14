dd2798339f741d19ba063831370904e3248ee981
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-06-14 22:29:17 +0200

    Fergekoding: Fiks tester som ble glemt

--- test/tipping_web/live/match_list_live_test.exs
+++ test/tipping_web/live/match_list_live_test.exs
@@ -15,11 +15,15 @@ defmodule TippingWeb.MatchListLiveTest do
     test "returns a list of tuples where the first element is a date and the second element is a list of matches and bets",
          %{user: user} do
       entries = MatchListLive.matches_with_bets_grouped_by_day(user)
-      assert match?([{%Date{}, [%{match: %WorldCup.Match{}, bet: _} | _]} | _], entries)
+
+      assert match?(
+               %{matches_by_day: [{%Date{}, [%{match: %WorldCup.Match{}, bet: _} | _]} | _]},
+               entries
+             )
     end
 
     test "the dates are in sorted order", %{user: user} do
-      entries = MatchListLive.matches_with_bets_grouped_by_day(user)
+      %{matches_by_day: entries} = MatchListLive.matches_with_bets_grouped_by_day(user)
       sorted_entries = Enum.sort_by(entries, &elem(&1, 0), Date)
       assert entries == sorted_entries
     end
@@ -27,6 +31,7 @@ defmodule TippingWeb.MatchListLiveTest do
     test "the matches' kickoff time is listed in the Europe/Oslo time zone", %{user: user} do
       matches =
         MatchListLive.matches_with_bets_grouped_by_day(user)
+        |> Map.get(:matches_by_day)
         |> Enum.flat_map(fn {_date, matches} -> matches end)
         |> Enum.map(& &1.match)
 
