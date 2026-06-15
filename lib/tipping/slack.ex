ee57dd2c73dc83755c63a12b9d2262da12a2c8ab
Erik André Jakobsen <erik.a.jakobsen@gmail.com>
2026-06-15 16:02:04 +0200

    Slackbot

---
+++ lib/tipping/slack.ex
@@ -0,0 +1,95 @@
+defmodule Tipping.Slack do
+  @greetings [
+    "¡Buenos días!",
+    "Goeiemôre!",
+    "좋은 아침이에요!",
+    "Dobré ráno!",
+    "Good morning!",
+    "Dobro jutro!",
+    "صباح الخير!",
+    "Guete Morge!",
+    "Bom dia!",
+    "Bonjou!",
+    "Madainn mhath!",
+    "Günaydın!",
+    "Guten Morgen!",
+    "Bon dia!",
+    "Bonjour!",
+    "Goedemorgen!",
+    "おはよう！",
+    "God morgon!",
+    "صبح بخیر!",
+    "Jàmm nga fanaane?",
+    "God morgen!",
+    "Xayrli tong!"
+  ]
+
+  def format_message(greeting, standings) do
+    %{
+      blocks: [
+        header(greeting),
+        formatted_points_table(standings)
+      ]
+    }
+  end
+
+  def random_greeting() do
+    Enum.random(@greetings)
+  end
+
+  defp header(greeting) do
+    %{
+      type: "header",
+      text: %{
+        type: "plain_text",
+        text: greeting,
+        emoji: true
+      },
+      level: 1
+    }
+  end
+
+  defp formatted_points_table(standings) do
+    header = [
+      %{
+        type: "raw_text",
+        text: "Position"
+      },
+      %{
+        type: "raw_text",
+        text: "Name"
+      },
+      %{
+        type: "raw_text",
+        text: "Points"
+      }
+    ]
+
+    rows =
+      Enum.map(standings, fn row ->
+        [
+          %{
+            type: "raw_number",
+            value: row.position,
+            text: Integer.to_string(row.position)
+          },
+          %{
+            type: "raw_text",
+            text: row.name
+          },
+          %{
+            type: "raw_number",
+            value: row.points,
+            text: Integer.to_string(row.points)
+          }
+        ]
+      end)
+
+    %{
+      type: "data_table",
+      caption: "Aidn standings",
+      page_size: 10,
+      rows: [header | rows]
+    }
+  end
+end
---
+++ test/tipping/slack_test.exs
@@ -0,0 +1,82 @@
+defmodule Tipping.SlackTest do
+  use ExUnit.Case, async: true
+
+  alias Tipping.Slack
+
+  describe "format_message/1" do
+    test "formats the given scores as a Slack JSON payload" do
+      result =
+        Slack.format_message("God morgen!", [
+          %{position: 1, name: "Amalie Skrede", points: 11},
+          %{position: 2, name: "Erik André Jakobsen", points: 9}
+        ])
+
+      assert result == %{
+               blocks: [
+                 %{
+                   type: "header",
+                   text: %{
+                     type: "plain_text",
+                     text: "God morgen!",
+                     emoji: true
+                   },
+                   level: 1
+                 },
+                 %{
+                   type: "data_table",
+                   caption: "Aidn standings",
+                   page_size: 10,
+                   rows: [
+                     [
+                       %{
+                         type: "raw_text",
+                         text: "Position"
+                       },
+                       %{
+                         type: "raw_text",
+                         text: "Name"
+                       },
+                       %{
+                         type: "raw_text",
+                         text: "Points"
+                       }
+                     ],
+                     [
+                       %{
+                         type: "raw_number",
+                         value: 1,
+                         text: "1"
+                       },
+                       %{
+                         type: "raw_text",
+                         text: "Amalie Skrede"
+                       },
+                       %{
+                         type: "raw_number",
+                         value: 11,
+                         text: "11"
+                       }
+                     ],
+                     [
+                       %{
+                         type: "raw_number",
+                         value: 2,
+                         text: "2"
+                       },
+                       %{
+                         type: "raw_text",
+                         text: "Erik André Jakobsen"
+                       },
+                       %{
+                         type: "raw_number",
+                         value: 9,
+                         text: "9"
+                       }
+                     ]
+                   ]
+                 }
+               ]
+             }
+    end
+  end
+end
